5cb6b2d301f93b9bfe1de91c1b0e2575a78ec25a
thinhnguyen1101 <thinhb1910709@student.ctu.edu.vn>
2022-09-24 16:22:32 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +11,20 @@ app.get('/', (req, res) => {
     res.json({massage: 'Welcome to contact book application.'});
 });
 
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
+
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+app.route('api/contacts/:id(\\d+)')
+    .get(contactController.fineOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+    
 module.exports = app;
+
+
+
 
