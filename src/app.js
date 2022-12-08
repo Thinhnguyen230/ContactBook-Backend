073e5cb11fa6c41a08924d143a305e26ca3842a3
thinhnguyen1101 <thinhb1910709@student.ctu.edu.vn>
2022-12-08 12:23:21 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,30 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
+const contactController = require("./controllers/contact.controller");
+const ApiError = require("./api-error");
 const app = express();
-const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({massage: 'Welcome to contact book application.'});
+app.get("/", (req, res) => {
+  res.json({ message: "Welcom to contact!" });
 });
 
-app.route('/api/contacts')
-    .get(contactController.findAll)
-    .post(contactController.create)
-    .delete(contactController.deleteAll);
+app
+  .route("/api/contacts")
+  .get(contactController.findAll)
+  .post(contactController.create)
+  .delete(contactController.deleteAll);
 
-app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+app.route("api/contacts/favorite").get(contactController.findAllFavorite);
 
-app.route('api/contacts/:id(\\d+)')
-    .get(contactController.fineOne)
-    .put(contactController.update)
-    .delete(contactController.delete);
-    
+app
+  .route("/api/contacts/:id")
+  .get(contactController.find0ne)
+  .put(contactController.update)
+  .delete(contactController.delete);
+
+// Handle 404 response.
+app.use((req, res, next) => {
+  // Handler for unknown route.
+  // Call next() to pass to the error handling middleware.
+  return next(new ApiError(404, "Resource not found"));
+});
+// Define error-handling middleware last, after other app.use() and routes calls.
+app.use((error, req, res, next) => {
+  // The centralized error handling middleware.
+  // In any route handler, calling next(error)
+  // will pass to this error handling middleware.
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
-
-
-
-
